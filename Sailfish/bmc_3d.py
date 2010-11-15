ef8e9d6e829ad3e3c7b687278e874061c94165b3
Alexandr Kuzmin <shurik@alex-desktop.(none)>
2010-11-15 15:13:28 -0700

    Modified three-dimensional example - doesn't work

--- Sailfish/bmc_3d.py
+++ Sailfish/bmc_3d.py
@@ -7,46 +7,47 @@ from sailfish import geo
 import optparse
 from optparse import OptionGroup, OptionParser, OptionValueError
 
-class GeoFE(geo.LBMGeo2D):
+class GeoFE(geo.LBMGeo3D):
 
     wall_width = 2
 
     def define_nodes(self):
-        hy, hx = np.mgrid[0:self.lat_ny, 0:self.lat_nx]
-        wall_map = np.logical_or(hy < self.wall_width, hy >= self.lat_ny-self.wall_width)
+        hz, hy, hx = np.mgrid[0:self.lat_nz,0:self.lat_ny, 0:self.lat_nx]
+        wall_map = np.logical_or(np.logical_or(hy < self.wall_width, hy >= self.lat_ny-self.wall_width),
+                                  np.logical_or(hx<self.wall_width,hx>=self.lat_nx-self.wall_width))
         self.set_geo(wall_map, self.NODE_WALL)
 
     def init_fields(self):
-        hy, hx = np.mgrid[0:self.lat_ny, 0:self.lat_nx]
+        hz, hy, hx = np.mgrid[0:self.lat_nz,0:self.lat_ny, 0:self.lat_nx]
         width = self.sim.options.iwidth
 
         self.sim.phi[:] = 1.0
-        self.sim.phi[np.logical_and(
-                       np.logical_and(hx >= (self.lat_nx-1)/3.0,
-                                      hx <= 2*(self.lat_nx-1)/3.0),
-                       np.logical_and(hy >= width-1 + self.wall_width, hy <= self.lat_ny-width-self.wall_width))] = -1.0
+        self.sim.phi[np.logical_and(np.logical_and(
+                       np.logical_and(hx >= width-1 + self.wall_width, hx <= self.lat_ny-width-self.wall_width),
+                       np.logical_and(hy >= width-1 + self.wall_width, hy <= self.lat_ny-width-self.wall_width)),
+                     np.logical_and(hz>=self.lat_nz/3.0,hz<=2*self.lat_nz/3.0))] = -1.0
         self.sim.rho[:] = 1.0
 
 class FESim(lbm.BinaryFluidFreeEnergy):
-    filename = 'fe_seperation_2d'
+    filename = 'fe_separation_3d'
 
     def __init__(self, geo_class, defaults={}):
-        settings = {'verbose': True, 'lat_nx': 3001,
-                    'lat_ny': 200+2, 'grid': 'D2Q9',
-                    'kappa': 0.04, 'Gamma': 1.0, 'A': 0.04,
+        settings = {'verbose': True, 'lat_nx': 32,'grid': 'D3Q19',
+                    'lat_ny': 32, 'lat_nz' :128 ,
+                    'kappa': 0.04, 'Gamma': 10.0, 'A': 0.04,
                     'scr_scale': 1,
                     'tau_a': 2.5, 'tau_b': 0.7, 'tau_phi': 1.0,
                     'bc_wall': 'halfbb',
-                    'periodic_x': True, 'periodic_y': True}
+                    'periodic_x': True, 'periodic_y': True,'periodic_z': True}
         settings.update(defaults)
 
         opts = []
-        opts.append(optparse.make_option('--force', dest='force', type='float', default=0.0))
+        opts.append(optparse.make_option('--force', dest='force', type='float', default=6.0e-6))
         opts.append(optparse.make_option('--iwidth', dest='iwidth', type='int', default=5))
 
         lbm.BinaryFluidFreeEnergy.__init__(self, geo_class, options=opts, defaults=settings)
 
-        self.add_body_force((self.options.force, 0.0), grid=0, accel=False)
+        self.add_body_force((0.0, 0.0, self.options.force), grid=0, accel=False)
 
         # Use the fluid velocity in the relaxation of the order parameter field,
         # and the molecular velocity in the relaxation of the density field.
