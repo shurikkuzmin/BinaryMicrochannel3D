1e4cc213480bc4f21fd8a0ecf0066e78d973eea9
Alexandr Kuzmin <shurik@alex-desktop.(none)>
2011-02-01 18:30:31 -0700

    Added some examples. Don't know how to put certain field to visualize.

--- Python/streamlines.py
+++ Python/streamlines.py
@@ -42,6 +42,100 @@ def read(name):
     #mlab.pipeline.vectors(v, mask_points=20, scale_factor=3.)
     mlab.pipeline.vector_cut_plane(v,plane_orientation='y_axes', mask_points=5)
     mlab.show()
+def read_vtk(name):
+    import vtk
+    import numpy
+
+    gridreader = vtk.vtkXMLStructuredGridReader()
+    gridreader.SetFileName(name)
+    gridreader.Update()
+
+    grid  = gridreader.GetOutput()
+    data  = grid.GetPointData()
+    points=grid.GetPoints()
+    dims  =grid.GetDimensions()
+    
+    
+    #print dims
+    #dims=list(dims)
+    #dims.reverse()
+    
+
+    phase= data.GetArray("Phase")
+    #velocity=data.GetArray("Velocity")
+    
+    contour=vtk.vtkContourFilter()
+    contour.SetInputConnection(gridreader.GetOutputPort())
+    contour.GenerateValues(50, phase.GetRange())
+    
+    contourMapper = vtk.vtkPolyDataMapper()
+    contourMapper.SetInputConnection(contour.GetOutputPort())
+    #contourMapper.SetScalarRange(gridreader.GetOutput().GetScalarRange())
+
+    #sr=vtk.vtkStructuredGridToPolyDataFilter(gridreader.GetOutputPort())
+    stlMapper = vtk.vtkPolyDataMapper()
+    #stlMapper.SetInput(data)
+    stlMapper.SetInputConnection(contour.GetOutputPort())
+
+    contour=vtk.vtkContourFilter()
+    
+    #reslice.SetInputConnection(reader.GetOutputPort())
+    #reslice.SetOutputDimensionality(2)
+    #reslice.SetResliceAxes(sagittal)
+    #reslice.SetInterpolationModeToLinear()
+
+
+
+    stlActor = vtk.vtkActor()
+    stlActor.SetMapper(stlMapper)
+
+    # Create the Renderer, RenderWindow, and RenderWindowInteractor
+    ren = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
+
+    # Add the actors to the render; set the background and size
+    ren.AddActor(stlActor)
+    ren.SetBackground(0.1, 0.2, 0.4)
+    renWin.SetSize(500, 500)
+
+    # Zoom in closer
+    ren.ResetCamera()
+    cam1 = ren.GetActiveCamera()
+    cam1.Zoom(1.4)
+
+    iren.Initialize()
+    renWin.Render()
+    iren.Start()
+
+    
+    #phase_array=[]
+    #velocity_array=[]
+    #phase.GetData(0,phase.GetNumberOfTuples(),0,0,phase_array)
+    #velocity.GetData(0,velocity.GetNumberOfTuples(),0,0,velocity_array)
+    
+    
+    
+    
+    #print velocity.shape
+    #print data.GetArrayName(0)
+    #print data.GetArrayName(1)
+    #print data.GetArrayName(2)
+    
+    #print velocity
+    #velx=velocity[:,0]
+    #vely=velocity[:,1]
+    #velz=velocity[:,2]
+    
+    #phase_numpy=phase.reshape(dims)
+    #velx_numpy =velx.reshape(dims)
+    #vely_numpy =vely.reshape(dims)
+    #velz_numpy =velz.reshape(dims)
+    
+    
+    
 
 def show(name,slice):
     from enthought.tvtk.api import tvtk
@@ -73,6 +167,6 @@ def show(name,slice):
     pylab.show()
 
 if __name__=="__main__":
-    name="/Users/shurik/Documents/Temp/Paraview/phase10000.vts"
-    read(name)
+    name="../Temp/phase040000.vts"
+    read_vtk(name)
     #show(name,300)
--- Python/velocity.py
+++ Python/velocity.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
+import os
+import numpy
+import pylab
+from enthought.tvtk.api import tvtk
+from enthought.mayavi import mlab
+import math
 
-def read(name):
-    import numpy
-    import pylab
+def Read(name):
     
     arr=numpy.load(name)
     
@@ -26,7 +30,151 @@ def read(name):
     
     pylab.show()
 
-if __name__=="__main__":
-    name="../../binary_microchannel/Sailfish/Grid/Results/202/grid400000.npz"
+def Read_VTI(name):
+ 
+
+    gridreader = tvtk.XMLStructuredGridReader()
+    gridreader.file_name = name
+    gridreader.update()
+
+    grid  = gridreader.output
+    data  = grid.point_data
+    points=grid.points
+    dims  =grid.dimensions
+    dims=dims.tolist()
+    dims.reverse()
+    phase= numpy.array(data.get_array("phi"))
+    velocity=numpy.array(data.get_array("v"))
+    velx=velocity[:,0]
+    vely=velocity[:,1]
+    velz=velocity[:,2]
+    phase_numpy=phase.reshape(dims)
+    velx_numpy =velx.reshape(dims)
+    vely_numpy =vely.reshape(dims)
+    velz_numpy =velz.reshape(dims)
     
-    read(name)
+    fig=mlab.figure()
+    src = mlab.pipeline.scalar_field(phase_numpy)
+    
+    v= mlab.pipeline.vector_field(velx_numpy,vely_numpy,velz_numpy)
+    vx=mlab.pipeline.scalar_field(velx_numpy)
+    vy=mlab.pipeline.scalar_field(vely_numpy)
+    vz=mlab.pipeline.scalar_field(velz_numpy)
+    
+    
+    extract=mlab.pipeline.extract_grid(src)
+    extract.set(z_min=1,z_max=dims[2]-2,y_min=1,y_max=dims[1]-2)
+    surf = mlab.pipeline.contour_surface(extract)
+ 
+    mlab.pipeline.image_plane_widget(vx, plane_orientation='x_axes', slice_index=250)
+    #mlab.pipeline.vectors(v, mask_points=20, scale_factor=3.)
+    mlab.pipeline.vector_cut_plane(v, mask_points=2, scale_factor=3)
+    mlab.show()
+
+def Read_Phase(name):
+    
+    print os.getcwd()
+    gridreader = tvtk.XMLImageDataReader()
+    gridreader.file_name = name
+    gridreader.update()
+
+    grid  = gridreader.output
+    data  = grid.point_data
+    dims  =grid.dimensions
+    dims=dims.tolist()
+    dims.reverse()
+    phase= numpy.array(data.get_array("phi"))
+    velocity=numpy.array(data.get_array("v"))
+    velx=velocity[:,0]
+    vely=velocity[:,1]
+    velz=velocity[:,2]
+    phase_numpy=phase.reshape(dims)
+    velx_numpy =velx.reshape(dims)
+    vely_numpy =vely.reshape(dims)
+    velz_numpy =velz.reshape(dims)
+    return phase_numpy,velx_numpy,vely_numpy,velz_numpy
+
+    
+def Show_Phase(phase_numpy,velx_numpy,vely_numpy,velz_numpy):
+    dims=phase_numpy.shape
+    
+    fig=mlab.figure()
+    src = mlab.pipeline.scalar_field(phase_numpy)
+    v= mlab.pipeline.vector_field(velx_numpy,vely_numpy,velz_numpy)
+    vx=mlab.pipeline.scalar_field(velx_numpy)
+    vy=mlab.pipeline.scalar_field(vely_numpy)
+    vz=mlab.pipeline.scalar_field(velz_numpy)
+  
+ 
+    mlab.outline()
+    mlab.orientation_axes()
+    extract=mlab.pipeline.extract_grid(src)
+    extract.set(x_min=1,x_max=dims[0]-2,y_min=1,y_max=dims[1]-2)
+    surf = mlab.pipeline.contour_surface(extract)
+
+    v_numpy=zip(velx_numpy,vely_numpy,velz_numpy) 
+    mlab.pipeline.image_plane_widget(vx, plane_orientation='x_axes', slice_index=250)
+    #mlab.pipeline.vectors(v, mask_points=20, scale_factor=3.)
+    mlab.pipeline.vector_cut_plane(v, mask_points=2, scale_factor=3)
+
+    
+def Analyze_Phase(name):
+    
+    #parameters of the binary liquid model
+    k=0.04
+    a=0.04
+    
+    phase_numpy,velx_numpy,vely_numpy,velz_numpy=Read_Phase(name)
+
+    Show_Phase(phase_numpy,velx_numpy,vely_numpy,velz_numpy)
+    dims=phase_numpy.shape
+
+    print dims
+    
+    center=phase_numpy[dims[0]/2,dims[1]/2,:]
+    z1 = numpy.min(numpy.where(center < 0.0))
+    z2 = numpy.max(numpy.where(center < 0.0))
+    if z1==0:
+        z2=numpy.min(numpy.where(center>0.0))+dims[2]
+        z1=numpy.max(numpy.where(center>0.0))
+    pylab.figure()
+    pylab.plot(center)
+    print z1,z2
+    
+    mid =((z1+z2)/2)%dims[2]
+    print mid
+    slice=phase_numpy[:,:,mid]
+    shape=slice.shape
+    prof_axis=slice[shape[0]/2,:]
+    prof_diag=numpy.diag(slice)
+    
+    #axis_zero=(1.0-Get_Zero(prof_axis)/(0.5*(shape[1]-2)))
+    #diag_zero=(math.sqrt(2.0)-math.sqrt(2.0)*Get_Zero(prof_diag)/(0.5*(shape[1]-2)))
+    
+    print "Velx",velx_numpy[dims[0]/2,dims[1]/2,mid]
+    print "Vely",vely_numpy[dims[0]/2,dims[1]/2,mid]
+    print "Velz",velz_numpy[dims[0]/2,dims[1]/2,mid]
+    
+    #Calculation of the capillary number
+    capillary=velx_numpy[dims[0]/2,dims[1]/2,mid]*(2.0/3.0)/math.sqrt(8.0*k*a/9.0)    
+    
+#    pylab.figure()
+#    pylab.imshow(phase_numpy[:,:,mid])
+#    pylab.figure()
+#    pylab.plot(prof_axis)
+#    pylab.figure()
+#    pylab.plot(prof_diag)
+
+    mlab.show()
+    
+    #print axis_zero,diag_zero
+    print "Capillary=",capillary
+
+    #return axis_zero,diag_zero,capillary
+
+
+
+if __name__=="__main__":
+    #name="../../binary_microchannel/Sailfish/Grid/Results/202/grid400000.npz"
+    name="../Sailfish/Asymmetric/50/asym200000_0.vti"
+    Analyze_Phase(name)
